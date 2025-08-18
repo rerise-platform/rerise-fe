02fff3888b6f8e5a346070ecca217fb105945577
imdodobby <163954285+imdodobby@users.noreply.github.com>
2025-08-18 12:38:55 +0900

    Update index.js
    충돌 해결

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import { store } from './store/store';
-import reportWebVitals from './reportWebVitals';
-
-// React 18의 새로운 createRoot API 사용
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// 앱을 렌더링하고 필요한 Provider들을 설정
-root.render(
-  <React.StrictMode>
-<<<<<<< Updated upstream
-    {/* Redux store를 전체 앱에 제공 */}
-    <Provider store={store}>
-      {/* React Router를 사용하여 클라이언트 사이드 라우팅 활성화 */}
-      <BrowserRouter>
-        {/* 메인 App 컴포넌트 */}
-        <App />
-      </BrowserRouter>
-    </Provider>
-=======
-    <App />
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -47,8 +19,6 @@ root.render(
       {/* 메인 App 컴포넌트 */}
       <App />
     </Provider>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   </React.StrictMode>
 );
 
