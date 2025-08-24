8b0f9579499f2fbd7e845d4cd69028366403495e
shinhyung <tlsgudpark1930@gmail.com>
2025-08-24 19:45:33 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import LoginPage from "./features/login/pages/LoginPage";
 import SignupPage from "./features/signup/pages/SignupPage";
 import MainPage from "./features/main/pages/MainPage";
 import EmotionPage from "./features/emotion/pages/EmotionPage";
-// ↓ 은서님 작업 컴포넌트들 추가
+// 작업 컴포넌트들 추가
 import MissionMainPage from "./features/mission/pages/MissionMainPage.jsx";
 import AdminPage from "./features/admin/pages/AdminPage.jsx";
 import TutorialPage from "./features/mission/pages/TutorialPage.jsx";
-
 import Navbar from "./shared/components/Navbar";
 import "./App.css";
 import TestPage from "./features/test/pages/TestPage.jsx";
@@ -19,7 +18,7 @@ import TestResultPage from "./features/test/pages/TestResultPage.jsx";
 function App() {
   const location = useLocation();
   
-  // ↓ 네비게이션 숨길 페이지 확장
+  // 네비게이션 숨길 페이지 확장
   const hideNavbarPages = [
     "/login",
     "/signup",
@@ -28,8 +27,9 @@ function App() {
     "/emotion",
     "/",
   ];
+  
   const shouldShowNavbar = !hideNavbarPages.includes(location.pathname);
-
+  
   return (
     <div className="app">
       <Routes>
@@ -38,23 +38,21 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/emotion" element={<EmotionPage />} />
-        {/* ↓ 은서님 작업 페이지들 추가 */}
+        {/* 작업 페이지들 추가 */}
         <Route path="/tutorial" element={<TutorialPage />} />
-        <Route path="/mission" element={<MissionMainPage />} />{" "}
-        {/* 실제 컴포넌트로 교체 */}
+        <Route path="/mission" element={<MissionMainPage />} />
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/test" element={<TestIntroPage />} />
         <Route path="/test/q" element={<TestPage />} />
         <Route path="/test/loading" element={<TestLoadingPage />} />
         <Route path="/test/result" element={<TestResultPage />} />
-        {/* ↓ 박신형 준비중 페이지들 유지 */}
+        {/* 준비중 페이지들 유지 */}
         <Route
           path="/recommendation"
           element={<div>추천 페이지 (준비중)</div>}
         />
         <Route path="/mypage" element={<div>마이 페이지 (준비중)</div>} />
       </Routes>
-
       
       {shouldShowNavbar && <Navbar />}
     </div>
