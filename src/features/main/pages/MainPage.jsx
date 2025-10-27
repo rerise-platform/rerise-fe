01830f1470b07d58802ac4c8d7e329dae83b8e55
SUHYUN YU <suhyun1219yu@gmail.com>
2025-10-28 04:21:51 +0900

    test

--- src/features/main/pages/MainPage.jsx
+++ src/features/main/pages/MainPage.jsx
@@ -324,8 +324,8 @@ const MainPage = () => {
         <Header>
           <Greeting>
             <GreetingText>
-              <Name>{mainData?.nickname || displayNickname || '디버그: 닉네임 없음'}</Name>
-              <Message>님, 안녕하세요!</Message>
+              <Name>{displayNickname || '사용자'}</Name>
+              <Message>님, 안녕하세요! 🚀 배포테스트 {new Date().toLocaleTimeString()}</Message>
             </GreetingText>
           </Greeting>
           <Character onClick={greetCharacter}>
